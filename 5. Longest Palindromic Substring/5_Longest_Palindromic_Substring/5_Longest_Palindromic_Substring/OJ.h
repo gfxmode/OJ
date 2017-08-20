0d4fb39ecc35ec902df3b5a6c3ee3e4fa2ba81fb
gfxmode <gfxmode@live.com>
2017-08-21 00:28:34 +0800

    1、新增OJ工程 2、新建5 longestPalindrome OJ工程

---
+++ 5. Longest Palindromic Substring/5_Longest_Palindromic_Substring/5_Longest_Palindromic_Substring/OJ.h
@@ -0,0 +1,17 @@
+#ifndef OJ_H
+#define OJ_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class OJ
+{
+public:
+	OJ(void);
+	~OJ(void);
+
+	string longestPalindrome(string s);
+};
+#endif OJ_H
