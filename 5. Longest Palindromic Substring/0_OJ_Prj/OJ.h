270cee61479137cbc2121494347f8ea9b950d5e5
gfxmode <gfxmode@live.com>
2017-08-25 21:33:27 +0800

    回文代码提交，复杂度o(n ^ 2)

--- 5. Longest Palindromic Substring/0_OJ_Prj/OJ.h
+++ 5. Longest Palindromic Substring/0_OJ_Prj/OJ.h
@@ -14,9 +14,13 @@ public:
 
     string longestPalindrome(string s)
     {
-        char arrResult[1000] = {0};
+        char arrResult[1005] = {0};             // 数组长度须大于1000。<=1000时，会访问越界
         char* arrInput = (char*)s.data();
         int strLen = strlen(arrInput);
+        if (1 == strLen)
+        {
+            return s;
+        }
 
         // 奇数回文数
         int maxOddThis = 0;
@@ -52,7 +56,31 @@ public:
             int maxLen = min(i, strLen - i - 1);
             int cnt = 0;                    // 回文数计数
             // 计算偶数回文数
+            for (int j = 0; j <= maxLen; j++)
+            {
+                if (arrInput[i - j] != arrInput[i + j + 1])
+                {
+                    break;
+                }
+                else
+                {
+                    cnt++;
+                }
+            }
 
+            if ((cnt > 0) && (cnt >= maxEvenThis) && (cnt > maxOddThis))
+            {
+                maxEvenThis = cnt;
+                memcpy(arrResult, arrInput + i - cnt + 1, 2*cnt);
+                arrResult[2* cnt + 1] = '\0';
+            }
+        }
+
+        // 未找到长度大于等于2的回文时，返回首字符，因单字符可认为是回文
+        if((0 == maxOddThis) && (0 == maxEvenThis))
+        {
+            arrResult[0] = arrInput[0];
+            arrResult[1] = '\0';
         }
 
         return arrResult;
