0c73d0d1585602ae1a76b52f23dc98d94a2b4b5b
gfxmode <gfxmode@live.com>
2017-09-10 14:37:02 +0800

    Tab替换为Space

--- 290. Word Pattern/OJ_Prj_VC6/OJ.h
+++ 290. Word Pattern/OJ_Prj_VC6/OJ.h
@@ -13,55 +13,55 @@ class OJ
 public:
     OJ(void){};
     ~OJ(void){};
-
+    
     bool wordPattern(string pattern, string str) 
-	{
+    {
         // 字符串分割
         vector<string> vecSplit;
-		string strDelim(" ");
+        string strDelim(" ");
         split(str, strDelim, &vecSplit);
-
-		// patter和分割后长度不等，返回false
-		if (pattern.size() != vecSplit.size())
-		{
-			return false;
-		}
-
-		// 存放pattern与分割值对应关系
+        
+        // patter和分割后长度不等，返回false
+        if (pattern.size() != vecSplit.size())
+        {
+            return false;
+        }
+        
+        // 存放pattern与分割值对应关系
         map<char, string> mapPattern;
-		map<string, bool> mapValueExsits;
-		for (int i = 0; i < pattern.size(); i++)
-		{
-			char chKey = pattern[i];
-			if (0 == mapPattern.count(chKey))
-			{
-				// abba, dog dog dog dog。多个key，对应相同value值处理
-				if(1 == mapValueExsits.count(vecSplit.at(i)))
-				{
-					return false;
-				}
-				mapPattern[chKey] = vecSplit.at(i);
-				mapValueExsits[vecSplit.at(i)] = true;
-			}
-			else
-			{
-				if (vecSplit.at(i) != mapPattern[chKey])
-				{
-					return false;
-				}
-			}
-		}
+        map<string, bool> mapValueExsits;
+        for (int i = 0; i < pattern.size(); i++)
+        {
+            char chKey = pattern[i];
+            if (0 == mapPattern.count(chKey))
+            {
+                // abba, dog dog dog dog。多个key，对应相同value值处理
+                if(1 == mapValueExsits.count(vecSplit.at(i)))
+                {
+                    return false;
+                }
+                mapPattern[chKey] = vecSplit.at(i);
+                mapValueExsits[vecSplit.at(i)] = true;
+            }
+            else
+            {
+                if (vecSplit.at(i) != mapPattern[chKey])
+                {
+                    return false;
+                }
+            }
+        }
         return true;
     }
     
     /*
-        函数名：split
-        功能：字符串分割
-        入参：s[IN]，待分割的字符串
-              delim[IN]，分割符
-              ret[OUT]，分割后的结果
-        返回值：void
-        说明：当字符串为空时，也会返回一个空字符串
+    函数名：split
+    功能：字符串分割
+    入参：s[IN]，待分割的字符串
+    delim[IN]，分割符
+    ret[OUT]，分割后的结果
+    返回值：void
+    说明：当字符串为空时，也会返回一个空字符串
     */  
     void split(std::string& s, std::string& delim,std::vector< std::string >* ret)  
     {  
