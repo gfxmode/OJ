5d3194f4f18e4ee04ab361c10210fbfcab14fe9a
gfxmode <gfxmode@live.com>
2017-09-23 00:13:57 +0800

    提交大数相乘代码，302/311用例跑过，还需修改0开头判断

---
+++ 02.LeetCode/43. Multiply Strings/OJ_Prj_VC6/OJ.h
@@ -0,0 +1,97 @@
+#ifndef OJ_H
+#define OJ_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class OJ
+{
+public:
+    OJ(void){};
+    ~OJ(void){};
+
+    /*  函数名：multiply
+        函数功能：大数相乘算法
+        说明：使用多项式相乘的方法计算结果，num1[i] * num2[j] = result[i + j]
+        入参：num1[IN]，乘数
+                num2[IN]，被乘数
+        返回值：string，相乘后的结果
+    */
+    string multiply(string num1, string num2) 
+    {
+        // num1、num2长度小于110，结果字符串最大长度110 * 110 = 12100
+        const int MAX_RESULT_LENGTH = 110 + 110;
+        int arrResult[MAX_RESULT_LENGTH] = {0};
+        int iLen1 = num1.size();
+        int iLen2 = num2.size();
+        /* Step1：多项式相乘
+            多项式相乘计算方法num1[i] * num2[j] = result[lenA + lenB - (i + j) - 2]
+            因字符串与数字存放顺序相反，从小到大遍历字符串，倒序存放在result数组中
+        */
+        for (int i = 0; i < iLen1; ++i)
+        {
+            for (int j = 0; j < iLen2; ++j)
+            {
+                int idxResult = iLen1 + iLen2 - (i + j) - 2;
+                arrResult[idxResult] = arrResult[idxResult] + ((num1[i] - '0') * (num2[j] - '0'));
+            }
+        }
+
+        /* Step2：结果从低到处理
+            多项式相乘计算方法num1[i] * num2[j] = result[i + j]
+            因字符串与数字存放顺序相反，从大到小遍历字符串，有进位时进位处理
+        */
+        int cntLen = 0;                 // 计算结果长度
+        int *ptrTmp = arrResult;
+        int iCarray = 0;
+        int i = 0;
+        for (i = 0; i <= (iLen1 + iLen2); ++i)
+        {
+            int iValue = ((iCarray + (*ptrTmp)) % 10);
+            iCarray = (iCarray + (*ptrTmp)) / 10;
+            *ptrTmp = iValue;
+            ++ptrTmp;
+            ++cntLen;
+            if (((cntLen >= (iLen1 + iLen2 - 1)) && (0 == *ptrTmp)))
+            {
+                break;
+            }
+        }
+
+        if (iCarray > 0)
+        {
+            *ptrTmp = iCarray;
+            ++cntLen;
+        }
+        /* Step3: Int数组转Char数组。
+        */
+        // 申请内存
+        char *ptrRet;
+        // 若结果为0，则直接输出0 + '\0'
+        if (0 == cntLen)
+        {
+            ptrRet = (char *)malloc(sizeof(char) * 2);
+            ptrRet[0] = '0';
+            ptrRet[1] = '\0';
+        }
+        else
+        {
+            ptrRet = (char *)malloc(sizeof(char) * cntLen + 1);
+
+            for (i = 0; i < cntLen; ++i)
+            {
+                // 因字符串与数组倒序存放，这里倒序存放
+                ptrRet[cntLen - i - 1] = arrResult[i] + '0';
+            }
+            ptrRet[cntLen] = '\0';
+        }
+        string strRet = ptrRet;
+        // 释放指针
+        free(ptrRet);
+
+        return strRet;
+    }
+};
+#endif OJ_H
