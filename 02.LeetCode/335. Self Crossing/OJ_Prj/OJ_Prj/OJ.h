180edebc310d88d58094d14cb046f3c8b4981a9b
gfxmode <gfxmode@live.com>
2017-10-30 00:45:50 +0800

    提交点计算方向枚举

--- 02.LeetCode/335. Self Crossing/OJ_Prj/OJ_Prj/OJ.h
+++ 02.LeetCode/335. Self Crossing/OJ_Prj/OJ_Prj/OJ.h
@@ -1,9 +1,18 @@
-#ifndef OJ_H
-#define OJ_H
+#ifndef __OJ_H__
+#define __OJ_H__
 
 #include <vector>
 
 using namespace std;
+
+// 方向枚举
+enum ENUM_DIRECTION
+{
+    DIRECTION_NORTH = 0,
+    DIRECTION_WEST,
+    DIRECTION_SOUTH,
+    DIRECTION_EAST,
+};
 
 class OJ
 {
@@ -13,7 +22,52 @@ public:
 
     bool isSelfCrossing(vector<int>& x)
     {
+        int sizeVec = x.size();
+        int sizeArr = sizeVec * sizeVec;    // 使用一维数组存放
+
+        // 声明一维数组，分配内存
+        /* (x, y)横纵坐标用于记录点是否被访问过
+            横纵坐标经x + y * sizeVec转换为一维数组存放
+        */
+        int *pArr = (int *)malloc(sizeof(int) * sizeArr);
+        if (NULL == pArr)
+        {
+            printf("pArr malloc failed");
+
+            return false;
+        }
+        // 数组初始化
+        memset(pArr, 0, sizeof(int) * sizeArr);
+
+        // 遍历vector，生成(x, y)点坐标，并判断一维数组中x + y * sizeVec是否访问过
+        int posX = 0, posY = 0;
+        int cntItem = 0;
+
+        vector<int>::iterator iter = x.begin();
+        for (; x.end() != iter; ++iter)
+        {
+            switch (cntItem % 4)
+            {
+                DIRECTION_NORTH:
+                    break;
+                DIRECTION_WEST:
+                    break;
+                DIRECTION_SOUTH:
+                    break;
+                DIRECTION_EAST:
+                    break;
+                default:
+                    break;
+            }
+            ++cntItem;
+        }
+
+
+        // 释放一维数组，释放内存
+        free(pArr);
+        pArr = NULL;
+
         return true;
     }
 };
-#endif OJ_H
+#endif __OJ_H__
